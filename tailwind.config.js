e84357a15fa374c071b5e5e84895aced93cfb2b1
nicolas ariel maldonado <nicolas.ariel.maldonado@gmail.com>
2021-06-28 22:53:20 -0300

    fixed tailwind not compiling on deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   purge: [
     './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx',
+    './src/**/*.js',
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
